e44977b338f096c701e449f7900e187b891d5e49
Ashley Maceli and Andrew Russo <andrew@>
2016-04-04 11:11:12 -0700

    Added more forms for user input

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 //business logic
-function Resident(name, section, restaurant, entertainment) {
+function Resident(name, section, restaurant, bar, entertainment, bridge) {
   this.residentName = name;
   this.residentSection = section;
   this.residentRestaurant = restaurant;
+  this.residentBar = bar;
   this.residentEntertainment = entertainment;
+  this.residentBridge = bridge;
 }
 
 // user interface logic
@@ -14,9 +16,11 @@ $(document).ready(function(){
     var inputNewName = $("input#newName").val();
     var inputSection = $("input#newSection").val();
     var inputRestaurant = $("input#newRestaurant").val();
+    var inputBar = $("input#newBar").val();
     var inputEntertainment = $("input#newEntertainment").val();
+    var inputBridge = $("input#newBridge").val();
 
-    var newResident = new Resident(inputNewName, inputSection, inputRestaurant, inputEntertainment);
+    var newResident = new Resident(inputNewName, inputSection, inputRestaurant, inputBar, inputEntertainment, inputBridge);
 
     $("ul#portlandInfo").append("<li><span class='resident'>" + newResident.residentName + "</span></li>");
 
@@ -26,12 +30,16 @@ $(document).ready(function(){
       $(".fullName").text(newResident.residentName);
       $(".portlandSection").text(newResident.residentSection);
       $(".favRestaurant").text(newResident.residentRestaurant);
+      $(".favBar").text(newResident.residentBar);
       $(".favEntertainment").text(newResident.residentEntertainment);
+      $(".favBridge").text(newResident.residentBridge);
     });
 
     $("input#newName").val("");
     $("input#newSection").val("");
     $("input#newRestaurant").val("");
+    $("input#newBar").val("");
     $("input#newEntertainment").val("");
+    $("input#newBridge").val("");
   });
 });
